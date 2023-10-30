1c147063959dde70e168be39dcff767c5d26042f
Andrew Wyte <77837619+ns-Awyte@users.noreply.github.com>
2023-10-30 20:11:52 +1100

    Update expand_drive.sh

--- expand_drive.sh
+++ expand_drive.sh
@@ -6,6 +6,6 @@ sudo growpart /dev/sda 3
 sudo pvresize /dev/sda3
 sudo lvresize -l+100%FREE --resizefs /dev/mapper/ubuntu--vg-ubuntu--lv
 sudo apt update
-sudo apt install linux-image-generic
+sudo apt install linux-image-generic -y
 sudo apt upgrade -y
 sudo reboot
